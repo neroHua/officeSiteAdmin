ec7756debf32de5893c349232d7d0b2a7598fdcc
neroHua <38644203+neroHua@users.noreply.github.com>
2022-03-14 20:06:32 +0800

    Update Login.vue

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,72 +1,54 @@
 <template>
-  <Form ref="loginForm" :model="form" :rules="rules" @keydown.enter.native="handleSubmit">
-    <FormItem prop="userName">
-      <Input v-model="form.userName" placeholder="请输入用户名">
-        <span slot="prepend">
-          <Icon :size="16" type="ios-person"></Icon>
-        </span>
-      </Input>
-    </FormItem>
-    <FormItem prop="password">
-      <Input type="password" v-model="form.password" placeholder="请输入密码">
-        <span slot="prepend">
-          <Icon :size="14" type="md-lock"></Icon>
-        </span>
-      </Input>
-    </FormItem>
-    <FormItem>
-      <Button @click="handleSubmit" type="primary" long>登录</Button>
-    </FormItem>
-  </Form>
+    <form>
+        <p>
+            用户名
+            <input
+            type="number"
+            name="userId"
+            v-model="loginForm.userId"
+            />
+        </p>
+        <p>
+            密码
+            <input
+            type="password"
+            name="password"
+            v-model="loginForm.password"
+            />
+        </p>
+        <button type="button" v-on:click="login">登录</button>
+    </form>
 </template>
 <script>
+import axios from 'axios'
 export default {
   name: 'LoginForm',
-  props: {
-    userNameRules: {
-      type: Array,
-      default: () => {
-        return [
-          { required: true, message: '账号不能为空', trigger: 'blur' }
-        ]
-      }
-    },
-    passwordRules: {
-      type: Array,
-      default: () => {
-        return [
-          { required: true, message: '密码不能为空', trigger: 'blur' }
-        ]
-      }
-    }
-  },
   data () {
     return {
-      form: {
-        userName: 'super_admin',
-        password: ''
-      }
-    }
-  },
-  computed: {
-    rules () {
-      return {
-        userName: this.userNameRules,
-        password: this.passwordRules
-      }
+      loginForm: {
+        userId: 1,
+        password: 'abc123',
+      },
     }
   },
   methods: {
-    handleSubmit () {
-      this.$refs.loginForm.validate((valid) => {
-        if (valid) {
-          this.$emit('on-success-valid', {
-            userName: this.form.userName,
-            password: this.form.password
-          })
-        }
-      })
-    }
+    login() {
+      axios
+        .post('/login',
+        JSON.stringify(this.loginForm),
+        {
+          headers:{
+            'content-Type': 'application/json;charset=utf-8'
+          }
+        })
+        .then(response => (
+          this.info = response
+        ))
+        .catch(failResponse => {
+          alert('系统繁忙')
+        })
+
+    },
   }
 }
 </script>
