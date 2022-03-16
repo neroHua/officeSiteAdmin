c94f67cc9aa67ca2e404fe2a37c59558e3ddaf90
neroHua <38644203+neroHua@users.noreply.github.com>
2022-03-16 18:20:32 +0800

    Update Login.vue

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -3,18 +3,16 @@
     <Card style="width:300px" id="loginCard" title="欢迎登陆">
       <Form ref="loginForm" :model="form" :rules="rules" @keydown.enter.native="login" label-position="right">
         <FormItem prop="userId">
-          <Input v-model="loginForm.userId" placeholder="请输入用户id">
+          <Input v-model="loginForm.userId" placeholder="请输入用户id"/>
             <span slot="prepend">
               <Icon :size="16" type="ios-person"></Icon>
             </span>
-          </Input>
         </FormItem>
         <FormItem prop="password">
-          <Input type="password" v-model="loginForm.password" placeholder="请输入密码">
+          <Input type="password" v-model="loginForm.password" placeholder="请输入密码"/>
             <span slot="prepend">
               <Icon :size="14" type="md-lock"></Icon>
             </span>
-          </Input>
         </FormItem>
         <FormItem>
           <Button @click="login" type="primary" long>登录</Button>
@@ -41,17 +39,16 @@ export default {
         .post('/login',
         JSON.stringify(this.loginForm),
         {
-          headers:{
+          headers: {
             'content-Type': 'application/json;charset=utf-8'
           }
         })
         .then(response => (
-          this.info = response
+          this.$router.push('/news')
         ))
         .catch(failResponse => {
           alert('系统繁忙')
         })
-
     },
   }
 }
