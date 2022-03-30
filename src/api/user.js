7c9476f6f8801f377947476aefd175fe24162e88
neroHua <38644203+neroHua@users.noreply.github.com>
2022-03-30 20:40:57 +0800

    Create user.js

--- src/api/user.js
+++ src/api/user.js
@@ -1,10 +1,11 @@
 import axios from '@/libs/api.request'
 
-export const login = ({ userName, password }) => {
+export const login = ({ userId, password }) => {
   const data = {
-    userName,
+    userId,
     password
   }
+
   return axios.request({
     url: 'login',
     data,
