cfb578a16d0239647e354756301204b0732013a1
neroHua <38644203+neroHua@users.noreply.github.com>
2022-03-30 20:41:39 +0800

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -17,13 +17,14 @@ export default {
    * @description api请求基础路径
    */
   baseUrl: {
-    dev: 'https://www.easy-mock.com/mock/5add9213ce4d0e69998a6f51/iview-admin/',
+    // dev: 'https://www.easy-mock.com/mock/5add9213ce4d0e69998a6f51/iview-admin/',
+    dev: 'api',
     pro: 'https://produce.com'
   },
   /**
    * @description 默认打开的首页的路由name值，默认为home
    */
-  homeName: 'home',
+  homeName: '/api/home',
   /**
    * @description 需要加载的插件
    */
