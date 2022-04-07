17bebb980a157838bbc89653653f1086e0eb2b89
neroHua <38644203+neroHua@users.noreply.github.com>
2022-04-07 17:57:02 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -10,18 +10,19 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable:{
-      "/login":{
-          target:"http://localhost:8080",
-          changeOrigin:true,
-          pathRewrite:{
-               '^/login':'/login'
-          }
+    proxyTable: {
+      '/api': {
+        target: 'http://localhost:8082/',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '/'
+        }
       }
     },
+
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8082, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
