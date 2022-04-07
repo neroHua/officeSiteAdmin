2c9e8e8bc36e9be90fb65836c6355a87b82fd7d8
neroHua <38644203+neroHua@users.noreply.github.com>
2022-04-07 17:20:30 +0800

    Create axios.js

---
+++ src/utils/axios.js
@@ -0,0 +1,63 @@
+
+import axios from 'axios'
+
+let httpService = axios.create({
+  baseURL: process.env.BASE_URL,
+  timeout: 5000
+})
+
+// 拦截请求
+httpService.interceptors.request.use(config => {
+  if (localStorage.getItem('token')) {
+    config.headers.token = localStorage.getItem('token')
+  }
+  return config;
+},err => {
+  Promise.reject(err);
+})
+
+// 拦截响应
+httpService.interceptors.response.use(response => {
+  console.log(response)
+  console.log('请求成功')
+  return response;
+},err => {
+  return Promise.reject(err);
+})
+
+// get请求的封装
+export function get(url, params={}, headers = {'Content-Type':'application/json'}) {
+  return new Promise((resolve,reject) => {
+    httpService({
+      url: url,
+      method: 'get',
+      params: params,
+      headers: headers,
+    }).then(res => {
+      resolve(res);
+    }).catch(err => {
+      reject(err);
+    })
+  })
+}
+
+// post请求封装 
+export function post(url, data = {}, headers = {'Content-Type':'application/json'}) {
+  return new Promise((resolve,reject) => {
+    httpService({
+      url: url,
+      method:'post',
+      data: JSON.stringify(data),
+      headers: headers
+    }).then(res => {
+      resolve(res);
+    }).catch(err => {
+      reject(err);
+    })
+  })
+}
+
+export default {
+	get,
+  post,
+}
