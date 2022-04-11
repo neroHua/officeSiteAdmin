449b23f724ef5edd102e49f840251943a860348d
neroHua <38644203+neroHua@users.noreply.github.com>
2022-04-11 17:23:11 +0800

    Update NewsAdd.vue

--- src/views/news/components/NewsAdd.vue
+++ src/views/news/components/NewsAdd.vue
@@ -1,15 +1,111 @@
 <template>
-  <div id="newsAdd">
-    <p>aaaaaaaa</p>
-  </div>
+  <Drawer
+    :title="titleTemp"
+    v-model="showTemp"
+    width="720"
+    :mask-closable="true"
+    :styles="styles"
+  >
+    <Form :model="formData">
+      <Row :gutter="32">
+        <Col span="12" />
+          <FormItem label="Name" label-position="top">
+            <Input
+              v-model="formData.name"
+              placeholder="please enter user name"
+            />
+            {{show}}
+          </FormItem>
+        <Col span="12" />
+          <FormItem label="Url" label-position="top">
+            <Input v-model="formData.url" placeholder="please enter url"/>
+              <span slot="prepend">http://</span>
+              <span slot="append">.com</span>
+          </FormItem>
+      </Row>
+      <FormItem label="Description" label-position="top">
+        <Input
+          type="textarea"
+          v-model="formData.desc"
+          :rows="4"
+          placeholder="please enter the description"
+        />
+      </FormItem>
+    </Form>
+    <div class="demo-drawer-footer">
+      <Button style="margin-right: 8px" @click="showTemp = false">Cancel</Button>
+      <Button type="primary" @click=" showTemp = false">Submit</Button>
+    </div>
+  </Drawer>
 </template>
 
 <script>
 export default {
-  name: 'NewsAdd'
-}
+  name: "NewsAdd",
+  props: {
+    show: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    title: {
+      type: String,
+      required: false,
+      default: '新增新闻',
+    },
+  },
+  data() {
+    return {
+      showTemp: true,
+      titleTemp: this.title,
+      styles: {
+        height: "calc(100% - 55px)",
+        overflow: "auto",
+        paddingBottom: "53px",
+        position: "static",
+        width: '300px',
+      },
+      formData: {
+        name: "",
+        url: "",
+        desc: "",
+      },
+    };
+  },
+  watch: {
+    show: {
+      handler(newValue, oldValue) {
+        console.log('show', oldValue, '->', newValue)
+        this.showTemp = this.show;
+      },
+      immediate: true,
+      deep: true,
+    },
+    title: {
+      handler(newValue, oldValue) {
+        console.log('title', oldValue, '->', newValue)
+        this.titleTemp = this.title;
+      },
+      immediate: true,
+      deep: true,
+    },
+    showTemp: {
+      handler(newValue, oldValue) {
+        console.log('showTemp', oldValue, '->', newValue)
+      },
+      immediate: true,
+      deep: true,
+    },
+    titleTemp: {
+      handler(newValue, oldValue) {
+        console.log('titleTemp', oldValue, '->', newValue)
+      },
+      immediate: true,
+      deep: true,
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
