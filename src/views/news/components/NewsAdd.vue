49c7b1c2a81f6251ced81e4f7813518bc3f83534
neroHua <38644203+neroHua@users.noreply.github.com>
2022-04-20 20:55:57 +0800

    Create NewsAdd.vue

--- src/views/news/components/NewsAdd.vue
+++ src/views/news/components/NewsAdd.vue
@@ -2,36 +2,29 @@
   <Drawer
     :title="titleTemp"
     v-model="showTemp"
-    width="720"
+    width="80%"
     :mask-closable="true"
     :styles="styles"
+    
   >
     <Form :model="formData">
       <Row :gutter="32">
         <Col span="12" />
-          <FormItem label="Name" label-position="top">
+          <FormItem label="标题" label-position="top">
             <Input
-              v-model="formData.name"
-              placeholder="please enter user name"
+              v-model="formData.title"
+              placeholder="请输入标题"
             />
-            {{show}}
           </FormItem>
         <Col span="12" />
-          <FormItem label="Url" label-position="top">
-            <Input v-model="formData.url" placeholder="please enter url"/>
-              <span slot="prepend">http://</span>
-              <span slot="append">.com</span>
-          </FormItem>
+          <Upload :before-upload="handleUploadCover" action="//jsonplaceholder.typicode.com/posts/">
+            <Button icon="ios-cloud-upload-outline">请上传封面</Button>
+          </Upload>
       </Row>
-      <FormItem label="Description" label-position="top">
-        <Input
-          type="textarea"
-          v-model="formData.desc"
-          :rows="4"
-          placeholder="please enter the description"
-        />
-      </FormItem>
     </Form>
+      <div class="editor-wrapper">
+        <div id="editorId"></div>
+      </div>
     <div class="demo-drawer-footer">
       <Button style="margin-right: 8px" @click="showTemp = false">Cancel</Button>
       <Button type="primary" @click=" showTemp = false">Submit</Button>
@@ -40,21 +33,20 @@
 </template>
 
 <script>
+import Editor from 'wangeditor'
+// import 'wangeditor/release/wangEditor.min.css'
 export default {
   name: "NewsAdd",
   props: {
     show: {
-      type: Boolean,
       required: true,
       default: false,
     },
     title: {
-      type: String,
       required: false,
       default: '新增新闻',
     },
     showCallBack: {
-      type: Function,
     },
   },
   data() {
@@ -62,30 +54,30 @@ export default {
       showTemp: true,
       titleTemp: this.title,
       styles: {
-        height: "calc(100% - 55px)",
-        overflow: "auto",
-        paddingBottom: "53px",
-        position: "static",
-        width: '300px',
+        height: 'calc(100% - 55px)',
+        overflow: 'auto',
+        paddingBottom: '53px',
+        position: 'static',
+        width: '100%',
       },
       formData: {
-        name: "",
-        url: "",
-        desc: "",
+        title: '', 
+        cover: '',
+        body: '',
       },
     };
   },
   watch: {
     show: {
       handler(newValue, oldValue) {
-        this.showTemp = this.show;
+        this.showTemp = newValue;
       },
       immediate: true,
       deep: false,
     },
     title: {
       handler(newValue, oldValue) {
-        this.titleTemp = this.title;
+        this.titleTemp = this.newValue;
       },
       immediate: true,
       deep: false,
@@ -98,8 +90,25 @@ export default {
       deep: false,
     },
   },
+  mounted() {
+    this.editor = new Editor(`#editorId`)
+    // this.editor.customConfig.onchangeTimeout = 200
+    this.editor.create()
+    let html = this.value || localStorage.editorCache
+    if (html) this.editor.txt.html(html)
+  },
+  methods: {
+    handleUploadCover: function(cover) {
+      this.formData.cover = cover
+      return false;
+    },
+  }
 };
 </script>
 
 <style>
+.ivu-drawer-body {
+  width: 900px;
+}
+
 </style>
