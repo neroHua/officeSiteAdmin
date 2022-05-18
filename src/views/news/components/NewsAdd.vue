c39c51f45e514b1e1c142c6a57309a4ed6af2916
neroHua <38644203+neroHua@users.noreply.github.com>
2022-05-18 10:54:45 +0800

    Update NewsAdd.vue

--- src/views/news/components/NewsAdd.vue
+++ src/views/news/components/NewsAdd.vue
@@ -8,27 +8,49 @@
   >
     <Form :model="formData">
       <Row :gutter="32">
-        <Col span="12" />
-          <FormItem label="标题" label-position="top">
-            <Input
-              v-model="formData.title"
-              placeholder="请输入标题"
-            />
-          </FormItem>
-        <Col span="12" />
-          <Upload :before-upload="handleBeforeUploadCover" action="//jsonplaceholder.typicode.com/posts/">
-            <Button icon="ios-cloud-upload-outline" v-if="!formData.cover">请上传封面</Button>
-            <img :src="formData.cover" v-if="formData.cover">
-          </Upload>
+        <FormItem label="标题" label-position="top">
+          <Input
+            v-model="formData.title"
+            placeholder="请输入标题"
+          />
+        </FormItem>
+      </Row>
+      <Row>
+        <div class="upload">
+          <template v-if="formData.cover">
+            <img :src="formData.cover">
+            <div class="cover">
+              <Icon type="ios-trash-outline" @click.native="handleRemoveCover"></Icon>
+            </div>
+          </template>
+          <template v-else>
+            <Upload
+              :show-upload-list="false"
+              :format="['jpg','jpeg','png']"
+              :max-size="2048"
+              :before-upload="handleBeforeUploadCover"
+              type="drag"
+              action="//jsonplaceholder.typicode.com/posts/"
+              style="display: inline-block;width:58px;">
+              <div style="width: 58px;height:58px;line-height: 58px;">
+                <Icon type="camera" size="20">请上传封面</Icon>
+              </div>
+            </Upload>
+          </template>
+        </div>
+      </Row>
+      <Row>
+        <div class="editor-wrapper">
+          <div id="editorId"></div>
+        </div>
+      </Row>
+      <Row>
+        <div class="demo-drawer-footer">
+          <Button style="margin-right: 8px" @click="showTemp = false">取消</Button>
+          <Button type="primary" @click="handlerSubmit">提交</Button>
+        </div>
       </Row>
     </Form>
-      <div class="editor-wrapper">
-        <div id="editorId"></div>
-      </div>
-    <div class="demo-drawer-footer">
-      <Button style="margin-right: 8px" @click="showTemp = false">Cancel</Button>
-      <Button type="primary" @click="handlerSubmit">Submit</Button>
-    </div>
   </Drawer>
 </template>
 
@@ -64,6 +86,7 @@ export default {
       },
       formData: {
         title: '测试标题1', 
+        // cover: 'http://localhost:8082/image/1652839558262chrome_WAaurZyh81.png',
         cover: null,
         body: '',
       },
@@ -99,40 +122,78 @@ export default {
       formData.append('file', files[0])
       uploadImageService(formData)
         .then(successResponse => {
-          console.log(successResponse.data.data)
           insert(successResponse.data.data)
         })
-        .catch(failResponse => {
-          console.log(failResponse)
-        })
-    }
+        .catch((failResponse) => {
+          console.log(failResponse);
+        });
+    };
 
-    this.editor.create()
+    this.editor.create();
   },
   methods: {
-    handleBeforeUploadCover: function(cover) {
-      this.formData.coverFile = cover
-      let formData = new FormData()
-      formData.append('file', cover)
+    handleBeforeUploadCover: function (cover) {
+      this.formData.coverFile = cover;
+      let formData = new FormData();
+      formData.append("file", cover);
       uploadImageService(formData)
-        .then(successResponse => {
-          this.formData.cover = successResponse.data.data
+        .then((successResponse) => {
+          this.formData.cover = successResponse.data.data;
         })
-        .catch(failResponse => {
-          console.log(failResponse)
-        })
+        .catch((failResponse) => {
+          console.log(failResponse);
+        });
       return false;
     },
-    handlerSubmit: function() {
-      this.formData.body = this.editor.txt.html()
-      this.newsAddSubmit(this.formData)
+    handleRemoveCover: function () {
+      this.formData.cover = null;
     },
-  }
+    handlerSubmit: function () {
+      this.formData.body = this.editor.txt.html();
+      this.newsAddSubmit(this.formData);
+    },
+  },
 };
 </script>
 
 <style>
 .ivu-drawer-body {
   width: 900px;
+}
+.upload {
+  display: inline-block;
+  width: 60px;
+  height: 60px;
+  text-align: center;
+  line-height: 60px;
+  border: 1px solid transparent;
+  border-radius: 4px;
+  overflow: hidden;
+  background: #fff;
+  position: relative;
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.2);
+  margin-right: 4px;
+}
+.upload img {
+  width: 100%;
+  height: 100%;
+}
+.cover {
+  display: none;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.6);
+}
+.upload:hover .cover {
+  display: block;
+}
+.cover i{
+  color: #fff;
+  font-size: 20px;
+  cursor: pointer;
+  margin: 0 2px;
 }
 </style>
