713678a246bf0b3ddc1663e088797dbd15bdb232
neroHua <38644203+neroHua@users.noreply.github.com>
2022-05-20 14:19:20 +0800

    Update News.vue

--- src/views/news/News.vue
+++ src/views/news/News.vue
@@ -10,15 +10,18 @@
         <Button type="primary" @click="openNewsAdd">新增新闻</Button>
         <Table :columns="columns" :data="data"></Table>
         <NewsAdd :show="newsAddProps.show" :showCallBack="newsAddShowCallBack" :newsAddSubmit="newsAddSubmit"></NewsAdd>
+        <NewsUpdate :show="newsUpdateProps.show" :newsUpdateData="newsUpdateProps.data" :showCallBack="newsUpdateShowCallBack" :newsUpdateSubmit="newsUpdateSubmit"></NewsUpdate>
     </div>
   </div>
 </template>
 <script>
-import { newsListService, newsAddService } from '../../service/news/newsService.js'
+import { newsListService, newsAddService, newsUpdateService } from '../../service/news/newsService.js'
 import NewsAdd from "@/views/news/components/NewsAdd"
+import NewsUpdate from "@/views/news/components/NewsUpdate"
 export default {
   components: {
-    NewsAdd ,
+    NewsAdd,
+    NewsUpdate,
   },
   data () {
     return {
@@ -48,17 +51,19 @@ export default {
                     props: {
                       type: 'primary',
                       size: 'small',
-                      onclick: function() {
-
-                      }
                     }
                   }, '查看'),
                   h('Button', {
                     props: {
                       type: 'primary',
-                      size: 'small'
+                      size: 'small',
+                    },
+                    on: {
+                      click: () => {
+                        this.openNewsUpdate(params.row);
+                      },
                     }
-                  }, '编辑')
+                  }, '编辑'),
               ]);
           }
         },
@@ -73,13 +78,16 @@ export default {
       ],
       newsAddProps: {
         show: false,
-      }
+      },
+      newsUpdateProps: {
+        show: false,
+        data: null,
+      },
     }
   },
   mounted: function() {
     newsListService({pageSize: 10, pageNumber: 1})
       .then(successResponse => {
-        console.log(successResponse)
         this.data = successResponse.data.data.dataList
       })
       .catch(failResponse => {
@@ -111,8 +119,34 @@ export default {
       .catch(failResponse => {
         console.log(failResponse)
       })
+    },
+    openNewsUpdate: function(params) {
+      console.log(params);
+      this.newsUpdateProps.show = true
+      this.newsUpdateProps.data = params 
+    },
+    newsUpdateShowCallBack: function(childShow) {
+      this.newsUpdateProps.show = childShow
+    },
+    newsUpdateSubmit: function(formData) {
+      newsUpdateService(formData)
+      .then(successResponse => {
+        this.$Message.info('修改新闻成功')
+        this.newsAddProps.show = false
+      })
+      .catch(failResponse => {
+        console.log(failResponse)
+      })
 
-    }
+      newsListService({pageSize: 10, pageNumber: 1})
+      .then(successResponse => {
+        console.log(successResponse)
+        this.data = successResponse.data.data.dataList
+      })
+      .catch(failResponse => {
+        console.log(failResponse)
+      })
+    },
   },
 }
 </script>
