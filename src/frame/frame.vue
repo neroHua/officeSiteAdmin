dce5528fda57716d1030379e9bc49e50b35cda07
neroHua <38644203+neroHua@users.noreply.github.com>
2022-06-14 15:08:35 +0800

    Create frame.vue

---
+++ src/frame/frame.vue
@@ -0,0 +1,94 @@
+<template>
+  <Layout style="height: 100%" class="frame">
+    <Sider hide-trigger collapsible :width="256" :collapsed-width="64" v-model="collapsed" class="left-sider" :style="{overflow: 'hidden'}">
+      <side-menu accordion ref="sideMenu" :active-name="$route.name" :collapsed="collapsed" @on-select="turnToPage" :menu-list="menuList">
+        <!-- 需要放在菜单上面的内容，如Logo，写在side-menu标签内部，如下 -->
+        <div class="logo-con">
+          <img v-show="!collapsed" :src="maxLogo" key="max-logo" />
+          <img v-show="collapsed" :src="minLogo" key="min-logo" />
+        </div>
+      </side-menu>
+    </Sider>
+    <Layout>
+      <Header class="header-con">
+        <header-bar :collapsed="collapsed" @on-coll-change="handleCollapsedChange">
+        </header-bar>
+      </Header>
+      <Content class="frame-content-con">
+        <Layout class="frame-layout-con">
+          <Content class="content-wrapper">
+            <keep-alive>
+              <router-view/>
+            </keep-alive>
+            <ABackTop :height="100" :bottom="80" :right="50" container=".content-wrapper"></ABackTop>
+          </Content>
+        </Layout>
+      </Content>
+    </Layout>
+  </Layout>
+</template>
+<script>
+import SideMenu from './components/side-menu'
+import HeaderBar from './components/header-bar'
+import User from './components/user'
+import ABackTop from './components/a-back-top'
+import minLogo from '../../static/images/logo-min.jpg'
+import maxLogo from '../../static/images/logo.jpg'
+import './frame.less'
+export default {
+  name: 'Frame',
+  components: {
+    SideMenu,
+    HeaderBar,
+    User,
+    ABackTop
+  },
+  data () {
+    return {
+      collapsed: false,
+      minLogo,
+      maxLogo,
+      isFullscreen: false
+    }
+  },
+  computed: {
+    userAvatar () {
+      return this.$store.state.user.avatarImgPath
+    },
+    menuList () {
+      return this.$store.getters.menuList
+    },
+  },
+  methods: {
+    turnToPage (route) {
+      console.log('bbbbbbbbb', route);
+      this.$router.push({
+        name,
+        params,
+        query
+      })
+    },
+    handleCollapsedChange (state) {
+      this.collapsed = state
+    },
+    handleCloseTag (res, type) {
+      if (type !== 'others') {
+        if (type === 'all') {
+          this.turnToPage(this.$config.homeName)
+        }
+      }
+      this.setTagNavList(res)
+    },
+    handleClick (item) {
+      this.turnToPage(item)
+    }
+  },
+  watch: {
+    '$route' (newRoute) {
+      this.$refs.sideMenu.updateOpenName(newRoute.name)
+    }
+  },
+  mounted () {
+  }
+}
+</script>
