e15b56ad6cc8a3d08a6b8f24f2209f5a77dc6454
neroHua <38644203+neroHua@users.noreply.github.com>
2022-06-14 15:04:53 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import router from './router'
 import iView from 'iview'
 import 'iview/dist/styles/iview.css'
+import store from './store'
 
 Vue.config.productionTip = false
 
@@ -14,6 +15,7 @@ Vue.use(iView)
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
