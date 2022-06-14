99362f9ff720544f6db1992f00819de4667cda7a
neroHua <38644203+neroHua@users.noreply.github.com>
2022-06-14 15:08:13 +0800

    Create frameConfig.js

---
+++ src/frame/frameConfig.js
@@ -0,0 +1,121 @@
+export const frameConfig = 
+  [
+    {
+      path: 'news',
+      name: 'news',
+      icon: 'ios-paper-outline',
+      child: [
+        {
+          path: 'news1',
+          name: 'news1',
+          icon: 'ios-paper-outline',
+          child: [
+            {
+              path: 'news11',
+              name: 'news11',
+              icon: 'ios-paper-outline',
+            },
+            {
+              path: 'news12',
+              name: 'news12',
+              icon: 'ios-paper-outline',
+            },
+          ],
+        },
+        {
+          path: 'news2',
+          name: 'news2',
+          icon: 'ios-paper-outline',
+          child: [
+            {
+              path: 'news21',
+              name: 'news21',
+              icon: 'ios-paper-outline',
+            },
+            {
+              path: 'news22',
+              name: 'news22',
+              icon: 'ios-paper-outline',
+            },
+          ],
+
+        },
+        {
+          path: 'news3',
+          name: 'news3',
+          icon: 'ios-paper-outline',
+          child: [
+            {
+              path: 'news31',
+              name: 'news31',
+              icon: 'ios-paper-outline',
+            },
+            {
+              path: 'news32',
+              name: 'news32',
+              icon: 'ios-paper-outline',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      path: 'more',
+      name: 'more',
+      icon: 'ios-paper-outline',
+      child: [
+        {
+          path: 'more1',
+          name: 'more1',
+          icon: 'ios-paper-outline',
+          child: [
+            {
+              path: 'more11',
+              name: 'more11',
+              icon: 'ios-paper-outline',
+            },
+            {
+              path: 'more12',
+              name: 'more12',
+              icon: 'ios-paper-outline',
+            },
+          ],
+        },
+        {
+          path: 'more2',
+          name: 'more2',
+          icon: 'ios-paper-outline',
+          child: [
+            {
+              path: 'more21',
+              name: 'more21',
+              icon: 'ios-paper-outline',
+            },
+            {
+              path: 'more22',
+              name: 'more22',
+              icon: 'ios-paper-outline',
+            },
+          ],
+
+        },
+        {
+          path: 'more3',
+          name: 'more3',
+          icon: 'ios-paper-outline',
+          child: [
+            {
+              path: 'more31',
+              name: 'more31',
+              icon: 'ios-paper-outline',
+            },
+            {
+              path: 'more32',
+              name: 'more32',
+              icon: 'ios-paper-outline',
+            },
+          ],
+        },
+      ],
+    },
+  ]
